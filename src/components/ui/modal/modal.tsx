2d71e3f57a6c53e2d79e2195f20a8fe207d84779
i-nikotin-THEGAME <i_nikotin@thegame.design>
2025-07-05 10:35:57 +0300

    Тесты cypress

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -9,7 +9,7 @@ import { ModalOverlayUI } from '@ui';
 export const ModalUI: FC<TModalUIProps> = memo(
   ({ title, onClose, children }) => (
     <>
-      <div className={styles.modal}>
+      <div className={styles.modal} data-cy='modal'>
         <div className={styles.header}>
           <h3
             className={
@@ -20,7 +20,7 @@ export const ModalUI: FC<TModalUIProps> = memo(
           >
             {title}
           </h3>
-          <button className={styles.button} type='button'>
+          <button className={styles.button} type='button' data-cy='modal-close'>
             <CloseIcon type='primary' onClick={onClose} />
           </button>
         </div>
